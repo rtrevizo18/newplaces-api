0ba58519dce87472911b1e1407ace14c9de321c3
rtrevizo18 <ricardotrevizo18@gmail.com>
2025-07-20 18:44:11 -0500

    refactor: Moved middleware logic to controller files

---
+++ src/controllers/places-controllers.js
@@ -0,0 +1,48 @@
+const HttpError = require("../models/http-error");
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516,
+    },
+    address: "20 W 34th St, New York, NY 10001",
+    creator: "u1",
+  },
+];
+
+const getPlaceById = (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+
+  if (!place) {
+    return next(
+      new HttpError("Could not find a place for the provided id", 404)
+    );
+  }
+
+  res.json({ place });
+};
+
+const getPlacesByUserId = (req, res, next) => {
+  const userId = req.params.uid;
+  const places = DUMMY_PLACES.filter((p) => {
+    return p.creator === userId;
+  });
+
+  if (places.length === 0) {
+    return next(
+      new HttpError("Could not find places for the provided user id", 404)
+    );
+  }
+
+  res.json({ places });
+};
+
+exports.getPlaceById = getPlaceById;
+exports.getPlacesByUserId = getPlacesByUserId;
