c281f2999f5313b32cfe15eb1205f6e515578be6
rtrevizo18 <ricardotrevizo18@gmail.com>
2025-07-21 12:24:04 -0500

    feat: Add necessary headers for frontend-backend communication

--- src/app.js
+++ src/app.js
@@ -8,13 +8,23 @@ const HttpError = require("./models/http-error");
 
 const MONGODB_USERNAME = process.env.MONGODB_USERNAME;
 const MONGODB_PASSWORD = process.env.MONGODB_PSWD;
-const MONGODB_DB_NAME = "places";
+const MONGODB_DB_NAME = "mern";
 
 const url = `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@placescluster0.ydq4ewd.mongodb.net/${MONGODB_DB_NAME}?retryWrites=true&w=majority&appName=PlacesCluster0`;
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
 
 app.use("/api/places", placesRoutes); // => /api/places
 
