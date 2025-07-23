9b237dc9c2a44249202279e79865fcf4d93c7645
rtrevizo18 <ricardotrevizo18@gmail.com>
2025-07-23 15:06:51 -0500

    fix: Updated fileUpload to include full path name

--- src/middleware/file-upload.js
+++ src/middleware/file-upload.js
@@ -1,21 +1,22 @@
 const multer = require("multer");
-const uuid = require("uuid/v1");
+const uuid = require("uuid");
+const path = require("path");
 
 const MIME_TYPE_MAP = {
   "image/png": "png",
-  "image/jpg": "jpg",
   "image/jpeg": "jpeg",
+  "image/jpg": "jpg",
 };
 
 const fileUpload = multer({
   limits: 500000,
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, "uploads/images");
+      cb(null, path.join(__dirname, "..", "uploads", "images"));
     },
     filename: (req, file, cb) => {
       const ext = MIME_TYPE_MAP[file.mimetype];
-      cb(null, uuid() + "." + ext);
+      cb(null, uuid.v1() + "." + ext);
     },
   }),
   fileFilter: (req, file, cb) => {
