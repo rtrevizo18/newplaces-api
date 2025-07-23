ddcaec933ca3ca759cdf1a535ad4cb45f3e4baa8
rtrevizo18 <ricardotrevizo18@gmail.com>
2025-07-23 13:46:10 -0500

    feat: Add multer image file configuration to POST signup route

--- src/routes/users-routes.js
+++ src/routes/users-routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { check } = require("express-validator");
+const fileUpload = require("../middleware/file-upload");
 
 const usersControllers = require("../controllers/users-controllers");
 
@@ -9,6 +10,7 @@ router.get("/", usersControllers.getUsers);
 
 router.post(
   "/signup",
+  fileUpload.single("image"),
   [
     check("name").notEmpty(),
     check("email").normalizeEmail().isEmail(),
